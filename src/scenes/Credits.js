6399ace469c5705c9477e984b4a117e9dc9c09a4
Aswin M.S <125258048+aswinms926@users.noreply.github.com>
2025-06-21 16:37:08 +0530

    Add files via upload

---
+++ src/scenes/Credits.js
@@ -0,0 +1,25 @@
+
+import BaseScene from './BaseScene';
+
+class CreditsScene extends BaseScene {
+
+  constructor(config) {
+    super('CreditsScene', {...config, canGoBack: true});
+
+    this.menu = [
+      {scene: null, text: 'Thank you for playing'}
+    ]
+  }
+
+  create() {
+    super.create();
+    this.createMenu(this.menu, () => {});
+  }
+}
+
+export default CreditsScene;
+
+
+
+// Create ScoreScene
+// Display Best score, you can get from the local storage
